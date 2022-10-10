965cc246449c6fbd8c0e8018910fc03417922026
mahan <m.mahmodi1380@gmail.com>
2022-10-10 22:07:07 +0330

    more styling and icons.

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,5 +1,9 @@
 import Link from 'next/link';
+import Button from '../ui/Button';
 import classes from './event-item.module.css';
+import AddressIcon from '../icons/address-icon';
+import DateIcon from '../icons/date-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
 const EventItem = (props) => {
   const { title, image, date, location, id } = props;
   const formatedDate = new Date(date).toLocaleDateString('en-US', {
@@ -17,15 +21,22 @@ const EventItem = (props) => {
           <div>
             <h2>{title}</h2>
             <div className={classes.date}>
+              <DateIcon />
               <time>{formatedDate}</time>
             </div>
             <div className={classes.address}>
+              <AddressIcon />
               <address>{formatedAddress}</address>
             </div>
           </div>
         </div>
         <div className={classes.actions}>
-          <Link href={exploreLink}>Explore Event</Link>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
