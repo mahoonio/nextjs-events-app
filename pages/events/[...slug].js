6b73a5cf46d6c22beceeb366c348a6b352957141
mahan <m.mahmodi1380@gmail.com>
2022-10-10 16:01:47 +0330

    setting up main pages.

---
+++ pages/events/[...slug].js
@@ -0,0 +1,9 @@
+const FilteredEventsPage = () => {
+  return (
+    <div>
+      <h1>filtered events page</h1>
+    </div>
+  );
+};
+
+export default FilteredEventsPage;
---
+++ pages/events/[eventId].js
@@ -0,0 +1,9 @@
+const EventDetailPage = () => {
+  return (
+    <div>
+      <h1>Event detail page</h1>
+    </div>
+  );
+};
+
+export default EventDetailPage;
