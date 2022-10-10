058452956dfd6ed39d40893b0081541a5467bd94
mahan <m.mahmodi1380@gmail.com>
2022-10-10 21:48:03 +0330

    styling components

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import classes from './event-item.module.css';
 const EventItem = (props) => {
   const { title, image, date, location, id } = props;
   const formatedDate = new Date(date).toLocaleDateString('en-US', {
@@ -9,21 +10,21 @@ const EventItem = (props) => {
   const exploreLink = `/events/${id}`;
   const formatedAddress = location.replace(', ', '\n');
   return (
-    <li>
+    <li className={classes.item}>
       <img src={'/' + image} alt={title} />
-      <div>
-        <div>
+      <div className={classes.content}>
+        <div className={classes.summary}>
           <div>
             <h2>{title}</h2>
-            <div>
+            <div className={classes.date}>
               <time>{formatedDate}</time>
             </div>
-            <div>
+            <div className={classes.address}>
               <address>{formatedAddress}</address>
             </div>
           </div>
         </div>
-        <div>
+        <div className={classes.actions}>
           <Link href={exploreLink}>Explore Event</Link>
         </div>
       </div>
--- components/events/EventList.js
+++ components/events/EventList.js
@@ -1,9 +1,9 @@
-import EventItem from '../EventItem';
-
+import EventItem from './EventItem';
+import classes from './event-list.module.css';
 const EventList = (props) => {
   const { events } = props;
   return (
-    <ul>
+    <ul className={classes.list}>
       {events.map((event) => (
         <EventItem
           title={event.title}
